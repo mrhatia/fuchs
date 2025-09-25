d5e15f35bc3b84154a83d20eee16f758ff5d4cb2
Hussnain <67601646+mrhatia@users.noreply.github.com>
2025-09-26 02:54:26 +0500

    Update theme

---
+++ wp-content/themes/fuchs/blocks/home-hero/block-hero.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Block Name: Faq
+ *
+ * The template for displaying the custom gutenberg block named Faq.
+ *
+ * @link https://www.advancedcustomfields.com/resources/blocks/
+ *
+ * @package Base Theme Package
+ * @since 1.0.0
+ */
+
+BaseTheme::block(
+	$block,
+	function ( $bst_block_id, $bst_block_name, $bst_block_fields, $bst_option_fields ) {
+
+		// Block variables.
+
+		?>
+
+		<?php
+	}
+);
+
