7b1ccccdcd4d54be750aa2c50d59260901b7a05b
Hussnain <67601646+mrhatia@users.noreply.github.com>
2025-10-02 13:43:57 +0500

    Update header.php

--- wp-content/themes/fuchs/header.php
+++ wp-content/themes/fuchs/header.php
@@ -115,7 +115,7 @@ $bst_var_tbar_btn      = $bst_option_fields['bst_var_tbar_btn'] ?? null;
 
 	<a class="skip-link screen-reader-text"
 		href="#page-section"><?php esc_html_e( 'Skip to content', 'basetheme_td' ); ?></a>
-	<!-- <div class="loader">
+	<div class="loader">
 		<div class="loader-inner">
 			<div class="spinner-text" data-title="CFuchs">CFuchs</div>
 		</div>
