345b44b169cf327fe95f236ef77870eb89b09914
Hussnain <raohussnainali@gmail.com>
2025-10-07 15:55:47 +0500

    Resolve Demo Issues

--- wp-content/themes/fuchs/partials/content-project.php
+++ wp-content/themes/fuchs/partials/content-project.php
@@ -14,7 +14,13 @@ $bst_var_post_tags       = get_the_tags( $bst_var_post_id );
 $bst_var_post_categories = get_categories( $bst_var_post_id );
 
 
-$bst_var_post_title = $bst_fields['bst_var_post_title'] ?? get_the_title();
+$bst_var_post_title = get_the_title();
+$bst_var_sngl_related_title = $bst_fields['bst_var_sngl_related_title'] ?? "WEITERE AKTUELLE PROJEKTE";
+$bst_var_sngl_variation = $bst_fields['bst_var_sngl_variation'] ?? null;
+$bst_var_sngl_related_projects = $bst_fields['bst_var_sngl_related_projects'] ?? null;
+
+
+
 
 ?>
 
@@ -27,69 +33,150 @@ $bst_var_post_title = $bst_fields['bst_var_post_title'] ?? get_the_title();
 	<div class="page-section">
 		<section>
 			<div class="wrapper">
-				<h3 class="">WEITERE AKTUELLE PROJEKTE</h3>
-				<div class="post-archive three-columns">
-					<?php
-						$args = array(
-							'post_type'      => 'project',
-							'posts_per_page' => 3,
-							'orderby'        => 'date',
-							'order'          => 'DESC',
-						);
+				<?php if ( $bst_var_sngl_related_title ) {  ?>
+					<h3 class=""><?php echo html_entity_decode( $bst_var_sngl_related_title ); ?></h3>
+				<?php } ?>
+				<?php
+				if($bst_var_sngl_variation === "manual"){
+					?>
 
-						$bst_query = new WP_Query( $args );
-
-						if ( $bst_query->have_posts() ) :
-							while ( $bst_query->have_posts() ) : $bst_query->the_post();
-							list( $bst_var_post_id, $bst_fields, $bst_option_fields ) = BaseTheme::defaults();
+					<div class="post-archive three-columns">
+						<?php
+							if ( $bst_var_sngl_related_projects ) {
 							?>
-								<article id="post-<?php the_ID(); ?>" <?php post_class( 'post-archive-box column' ); ?>>
-									<div class="post-archive-box-img post-image">
-										<a href="<?php the_permalink(); ?>">
-											<?php
-												if ( ! has_post_thumbnail( $bst_var_post_id ) ) {
-													echo '<img class="" src="' . esc_url( get_template_directory_uri() ) . '/assets/build/images/admin/defaults/default-image.webp" >';
-												} else {
-													echo get_the_post_thumbnail(
-														$bst_var_post_id,
-														'thumb_1000',
-													);
-												}
-											?>
-										</a>
-									</div>
-									<div class="post-content">
-										<div class="post-box-meta d-flex justify-content-between">
-											<div class="ac-post-cat">
-												<?php echo get_the_date( 'F j, Y' ); ?>
+								<?php
+									foreach( $bst_var_sngl_related_projects as $key =>  $project_id ){
+										list( $bst_var_post_id, $bst_fields, $bst_option_fields ) = BaseTheme::defaults($project_id);
+										$terms = get_the_terms( $bst_var_post_id, 'category' );
+
+
+										?>
+
+										<article id="post-<?php the_ID($bst_var_post_id); ?>" <?php post_class( 'post-archive-box column' ); ?>>
+											<div class="post-archive-box-img post-image">
+												<a href="<?php the_permalink($bst_var_post_id); ?>">
+													<?php
+														if ( ! has_post_thumbnail( $bst_var_post_id ) ) {
+															echo '<img class="" src="' . esc_url( get_template_directory_uri() ) . '/assets/build/images/admin/defaults/default-image.webp" >';
+														} else {
+															echo get_the_post_thumbnail(
+																$bst_var_post_id,
+																'thumb_1000',
+															);
+														}
+													?>
+												</a>
 											</div>
-										</div>
-										<div class="post-archive-box-title post-title">
-											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> </h4>
-										</div>
-										<div class="bottom-section-button">
-											<a href="<?php the_permalink(); ?>">
-												<span>
-													Read More
-												</span>
-												<div class="plus-button">
-													+
+											<div class="post-content">
+												<div class="post-box-meta d-flex justify-content-between">
+													<div class="ac-post-cat">
+														<?php
+															if ( $terms && ! is_wp_error( $terms ) ) {
+																foreach ( $terms as $term ) {
+																	echo '<a href="' . esc_url( get_term_link( $term ) ) . '">' . esc_html( $term->name ) . '</a> ';
+																}
+															}
+														?>
+													</div>
 												</div>
+												<div class="post-archive-box-title post-title">
+													<h4><a href="<?php the_permalink($bst_var_post_id); ?>"><?php echo get_the_title($bst_var_post_id); ?></a> </h4>
+												</div>
+												<div class="bottom-section-button">
+													<a href="<?php the_permalink($bst_var_post_id); ?>">
+														<span>
+															Read More
+														</span>
+														<div class="plus-button">
+															+
+														</div>
+													</a>
+												</div>
+											</div>
+										</article>
+
+										<?php
+									}
+								?>
+							<?php
+							} ?>
+
+
+
+					</div>
+
+				<?php } else { ?>
+					<div class="post-archive three-columns">
+						<?php
+							$args = array(
+								'post_type'      => 'project',
+								'posts_per_page' => 3,
+								'orderby'        => 'date',
+								'order'          => 'DESC',
+							);
+
+							$bst_query = new WP_Query( $args );
+
+							if ( $bst_query->have_posts() ) :
+								while ( $bst_query->have_posts() ) : $bst_query->the_post();
+								list( $bst_var_post_id, $bst_fields, $bst_option_fields ) = BaseTheme::defaults();
+								$terms = get_the_terms( $bst_var_post_id, 'category' );
+
+								?>
+									<article id="post-<?php the_ID(); ?>" <?php post_class( 'post-archive-box column' ); ?>>
+										<div class="post-archive-box-img post-image">
+											<a href="<?php the_permalink(); ?>">
+												<?php
+													if ( ! has_post_thumbnail( $bst_var_post_id ) ) {
+														echo '<img class="" src="' . esc_url( get_template_directory_uri() ) . '/assets/build/images/admin/defaults/default-image.webp" >';
+													} else {
+														echo get_the_post_thumbnail(
+															$bst_var_post_id,
+															'thumb_1000',
+														);
+													}
+												?>
 											</a>
 										</div>
-									</div>
-								</article>
+										<div class="post-content">
+											<div class="post-box-meta d-flex justify-content-between">
+												<div class="ac-post-cat">
+													<?php
+														if ( $terms && ! is_wp_error( $terms ) ) {
+															foreach ( $terms as $term ) {
+																echo '<a href="' . esc_url( get_term_link( $term ) ) . '">' . esc_html( $term->name ) . '</a> ';
+															}
+														}
+													?>
+												</div>
+											</div>
+											<div class="post-archive-box-title post-title">
+												<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> </h4>
+											</div>
+											<div class="bottom-section-button">
+												<a href="<?php the_permalink(); ?>">
+													<span>
+														Read More
+													</span>
+													<div class="plus-button">
+														+
+													</div>
+												</a>
+											</div>
+										</div>
+									</article>
 
-							<?php endwhile;
-							wp_reset_postdata();
-						else :
-							echo '<p>No projects found.</p>';
-						endif;
-						?>
+								<?php endwhile;
+								wp_reset_postdata();
+							else :
+								echo '<p>No projects found.</p>';
+							endif;
+							?>
 
 
 
-				</div>
+					</div>
+				<?php } ?>
 			</div>
 		</section>
 	</div>
